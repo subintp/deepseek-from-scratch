b20bad1ce0d277c7b9ee527ada0723f39faa226d
Subin T P <subinthattaparambil@gmail.com>
2025-11-19 02:58:49 +0400

    add self attention

--- attention/self_attention.py
+++ attention/self_attention.py
@@ -1,5 +1,27 @@
 
+import torch
+import torch.nn as nn
 
-class SelfAttention:
-    def __init__(self):
-        print("SelfAttention initialized")
+class SelfAttention(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        print("self attention initialized")
+        self.d_in = d_in
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias = qkv_bias)
+        
+
+    def forward(self,x):
+        print("self attention forward")
+        keys = self.W_key(x)
+        values = self.W_value(x)
+        queries = self.W_query(x)
+
+        attention_scores = queries @ keys.T
+        attention_weights = torch.softmax(attention_scores/torch.sqrt(torch.tensor(self.d_in)), dim = -1)
+        context_vector = attention_weights @ values
+        return context_vector
+        
+        
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-"""DeepSeek from scratch - Demonstrating modular architecture."""
-
+import torch
 from attention import SelfAttention
 
 def main():
-    print("Hello from deepseek-from-scratch!")
-    attention = SelfAttention()
+    torch.manual_seed(789)
+
+    inputs = torch.tensor([
+      [0.43, 0.15, 0.89], # Your     (x^1)
+      [0.55, 0.87, 0.66], # journey  (x^2)
+      [0.57, 0.85, 0.64], # starts   (x^3)
+      [0.22, 0.58, 0.33], # with     (x^4)
+      [0.77, 0.25, 0.10], # one      (x^5)
+      [0.05, 0.80, 0.55]  # step     (x^6)
+    ])
+
+    d_in = 3
+    d_out = 2
+
+    sa_v2 = SelfAttention(d_in, d_out)
+    print(sa_v2(inputs))
 
 
 if __name__ == "__main__":
